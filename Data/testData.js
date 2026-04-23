cd7849b81efa01be10ba38410ce3f7f12cb84887
Nirav8734 <166927153+Nirav8734@users.noreply.github.com>
2026-04-23 16:11:58 +0530

    New Commit

--- Data/testData.js
+++ Data/testData.js
@@ -2,12 +2,11 @@ export const TestData = {
     login: {
         url: 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login',
         validuser: {
-            username: 'niravprajapati1309@gmail.com',
-            password: '8734986314',
+            username: 'Admin',
+            password: 'admin123',
         },
-        invaliduser: {
-            username: 'xyaz@gmail.io',
-            password: '12345678',
+        //invaliduser: {
+            //username: 'xyaz@gmail.io',
+            //password: '12345678',
         },
-    },
-}
+    }
