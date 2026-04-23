aba2cf1b04348f66bd6fe58ec00306736c4dbdea
Nirav8734 <166927153+Nirav8734@users.noreply.github.com>
2026-04-23 15:27:29 +0530

    New Commit

--- Locators/loginLocators.js
+++ Locators/loginLocators.js
@@ -5,9 +5,9 @@ class locator {
         this.loginlink = page.locator('#nav-link-accountList')
         //this.usernameinput= page.locator('//input[@id="ap_email_login"]')
         this.usernameinput= page.locator('#ap_email')
-        this.buttoninput= page.locator('//input[@class="a-button-input"]')
-        this.passwordinput= page.locator('//input[@id="ap_password"]')
-        this.signininput= page.locator('//input[@id="signInSubmit"]')
+        this.buttoninput= page.locator('#continue-announce')
+        this.passwordinput= page.locator('#ap_password')
+        this.signininput= page.locator('#signInSubmit')
     }
 }
 module.exports = {locator};
