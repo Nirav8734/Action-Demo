4e3dbfa8daf760d695c0356c03b190383fcb9734
Nirav8734 <166927153+Nirav8734@users.noreply.github.com>
2026-04-23 15:37:20 +0530

    New Commit

--- Pages/loginPage.js
+++ Pages/loginPage.js
@@ -15,11 +15,11 @@ class loginPage {
     }
 
     async login(username , password){
-        await this.help.click(this.loc.loginlink);
-        await this.help.fill(this.loc.usernameinput, username)
-        await this.help.click(this.loc.buttoninput);
-        await this.help.fill(this.loc.passwordinput, password);
-        await this.help.click(this.loc.signininput);
+        await this.page.click(this.loc.loginlink);
+        await this.page.fill(this.loc.usernameinput, username)
+        await this.page.click(this.loc.buttoninput);
+        await this.page.fill(this.loc.passwordinput, password);
+        await this.page.click(this.loc.signininput);
     }
 }
 
