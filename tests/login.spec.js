3b4c2592bc023486b36377667ea919fe6d752dc6
Nirav8734 <166927153+Nirav8734@users.noreply.github.com>
2026-04-27 10:13:46 +0530

    jenkins second commit

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -7,7 +7,7 @@ test('verify valid login', async ({ page }) => {
     const login = new loginPage(page);
    
     await login.gotologinpage(TestData.login.url);
-    await page.waitForTimeout(5000);
+    //await page.waitForTimeout(5000);
 
     await login.login(TestData.login.validuser.username,
         TestData.login.validuser.password);
